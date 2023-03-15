5eb0e29668d7615e681f9a3ae2bbc9508c3fafcb
Abbas Aziz <abbas9111@gmail.com>
2023-03-15 11:30:13 +0500

    first commit

---
+++ models.py
@@ -0,0 +1,41 @@
+import os
+from sqlalchemy import Column, String, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:root@localhost:5432/postgres'
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.app_context().push() 
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+'''
+Person
+Have title and release year
+'''
+class Person(db.Model):  
+  __tablename__ = 'People'
+
+  id = Column(db.Integer, primary_key=True)
+  name = Column(String)
+  catchphrase = Column(String)
+
+  def __init__(self, name, catchphrase=""):
+    self.name = name
+    self.catchphrase = catchphrase
+
+  def format(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'catchphrase': self.catchphrase}
