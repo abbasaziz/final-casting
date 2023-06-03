afee1f171e0c43229b3da1ada39099a448b9dd55
Abbas Aziz <77748051+abbasaziz@users.noreply.github.com>
2023-06-03 12:15:47 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export DATABASE_URL="postgresql://postgres@localhost:5432/{name of your database}"
 export EXCITED="true"
-echo "setup.sh script executed successfully!"
+echo "setup.sh script has been executed successfully!"
